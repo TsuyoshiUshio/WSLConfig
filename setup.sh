4494d1566347d80b7ca491cfe5e3036d0fbe8ac3
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-08-12 22:38:28 -0700

    Adding all shell files

--- setup.sh
+++ setup.sh
@@ -7,6 +7,16 @@ bash ./install_azurecli.sh
 # This is interfactive mode
 az login 
 
+bash ./install_node.sh
+
+bash ./install_aks.sh
+
+bash ./install-dotnet.sh
+
+bash ./install-azure-functions-core-tools.sh
+
+bash ./custom_config.sh
+
 
 
 
